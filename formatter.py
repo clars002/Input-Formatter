8ae6d339264113bbbf54e69bd470800e28752a87
Caelan Larsen <clars002@odu.edu>
2024-11-26 17:57:08 -0500

    initial pass

--- formatter.py
+++ formatter.py
@@ -1,5 +1,44 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--input",
+        type=str,
+        default="input/optdigits-orig.txt",
+        help="Input file to be formatted."
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    print("Hello, world!")
+    args = parse_arguments()
+
+    predictions = open("output/predictions.txt", "w")
+    observations = open("output/observations.txt", "w")
+
+    with open(args.input) as input_file:
+        text = input_file.readlines()
+        for line in text:
+            line = line.rstrip()
+            if line[0] == ' ':
+                observations.write(line[1] + '\n')
+                predictions.write('\n')
+            else:
+                for i in range(len(line)):
+                    predictions.write(line[i] + ' ')
+
+    predictions.close()
+    observations.close()
+
+
+# def process_line(str):
+#     if line[0] == ' ':
+        
+
 
 if __name__ == "__main__":
     main()
